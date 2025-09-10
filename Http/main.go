7befc8ca87512016c31af48fed1631f8ca240ed7
Indroneel007 <2021ume1106@mnit.ac.in>
2025-09-10 08:58:03 +0530

    tcp connection

--- Http/main.go
+++ Http/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"fmt"
 	//"io"
-	"io/ioutil"
+	//"io/ioutil"
 	"log"
-	"os"
+	//"os"
+	"net"
 )
 
 func main() {
 	//println("Hello, World!")
-	file, err := os.Open("message.txt")
+	/*file, err := os.Open("message.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -21,5 +22,27 @@ func main() {
 	}()
 	//tyjtyjty
 	b, err := ioutil.ReadAll(file)
-	fmt.Printf("Data as string: %s\n", b)
+	fmt.Printf("Data as string: %s\n", b)*/
+
+	listener, err := net.Listen("tcp", ":42069")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Connection from server connected\n")
+
+		_, err = conn.Write([]byte("Hello from server!\n"))
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		conn.Close()
+		fmt.Printf("Connection from server closed\n")
+	}
 }
