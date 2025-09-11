b570537dbcf11bd81aa3da98e70bb038075c596c
Indroneel007 <2021ume1106@mnit.ac.in>
2025-09-11 17:19:20 +0530

    request line and tests

---
+++ Http/internal/request/request.go
@@ -0,0 +1,15 @@
+package request
+
+import "io"
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error)
