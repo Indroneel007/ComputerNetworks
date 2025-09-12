c4b6d99448b8b1d11e314dc17b8ce094562ae567
Indroneel007 <2021ume1106@mnit.ac.in>
2025-09-12 15:13:21 +0530

    parsing

--- Http/internal/request/request.go
+++ Http/internal/request/request.go
@@ -1,6 +1,10 @@
 package request
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"strings"
+)
 
 type RequestLine struct {
 	HttpVersion   string
@@ -12,4 +16,43 @@ type Request struct {
 	RequestLine RequestLine
 }
 
-func RequestFromReader(reader io.Reader) (*Request, error)
+var ERROR_BAD_START_LINE = fmt.Errorf("bad start line")
+var SEPARATOR = "\r\n"
+
+func ParseRequestLine(b string) (*RequestLine, string, error) {
+	idx := strings.Index(b, SEPARATOR)
+	if idx == -1 {
+		return nil, "", nil
+	}
+
+	startLine := b[:idx]
+	restOfMesg := b[idx+len(SEPARATOR):]
+
+	parts := strings.Split(startLine, " ")
+	if len(parts) != 3 {
+		return nil, "", ERROR_BAD_START_LINE
+	}
+
+	return &RequestLine{
+		Method:        parts[0],
+		RequestTarget: parts[1],
+		HttpVersion:   parts[2],
+	}, restOfMesg, nil
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	str := string(data)
+	reqLine, _, err := ParseRequestLine(str)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Request{
+		RequestLine: *reqLine,
+	}, nil
+}
